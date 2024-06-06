2950e32edc3d5ac8df03b57bbdb86d63c07cf80d
pikol93 <piotrdampc93@gmail.com>
2024-06-06 17:55:25 +0200

    PI-1 Added health check

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() {
+    HttpServer::new(|| App::new().service(health_check))
+        .bind(("0.0.0.0", 8080))
+        .expect("Binding server")
+        .run()
+        .await
+        .expect("Running server");
+}
+
+#[get("/")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }

f9411e0c35346a07cbf31b43a1f4b78aedebf032
pikol93 <piotrdampc93@gmail.com>
2024-06-06 17:59:50 +0200

    PI-1 Modified backend to use error handling instead of expect()

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,14 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use color_eyre::Result;
 
 #[actix_web::main]
-async fn main() {
+async fn main() -> Result<()> {
     HttpServer::new(|| App::new().service(health_check))
-        .bind(("0.0.0.0", 8080))
-        .expect("Binding server")
+        .bind(("0.0.0.0", 8080))?
         .run()
-        .await
-        .expect("Running server");
+        .await?;
+    
+    Ok(())
 }
 
 #[get("/")]

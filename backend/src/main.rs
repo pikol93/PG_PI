9973523b4bba283a5b57147324e6bf994f988c15
pikol93 <piotrdampc93@gmail.com>
2024-06-12 16:39:51 +0200

    PI-4 Added logger initialization

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,36 @@
+use actix_web::middleware::Logger;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use color_eyre::Result;
+use dotenvy::dotenv;
+use tracing::{info, instrument};
+use tracing_subscriber::filter::EnvFilter;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    HttpServer::new(|| App::new().service(health_check))
+    initialize_logger()?;
+
+    HttpServer::new(|| App::new().wrap(Logger::default()).service(health_check))
         .bind(("0.0.0.0", 8080))?
         .run()
         .await?;
-    
+
     Ok(())
 }
 
 #[get("/")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
+}
+
+#[instrument]
+fn initialize_logger() -> Result<()> {
+    let _ = dotenv()?;
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    info!("Initialized logger.");
+
+    Ok(())
 }

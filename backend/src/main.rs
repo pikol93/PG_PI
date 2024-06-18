42a7219f8c3721da656f88487acd98f8fb6e814a
pikol93 <piotrdampc93@gmail.com>
2024-06-18 21:06:55 +0200

    PI-5 Removed unnecessary function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,13 +5,17 @@ use actix_web::middleware::Logger;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use color_eyre::Result;
 use dotenvy::dotenv;
-use tracing::{debug, info, instrument};
+use tracing::debug;
 use tracing_subscriber::filter::EnvFilter;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
     let _ = dotenv();
-    initialize_logger();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     let configuration = Configuration::new_from_env()?;
     debug!("Read environment configuration: {:?}", configuration);
 
@@ -26,13 +30,4 @@ async fn main() -> Result<()> {
 #[get("/")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
-}
-
-#[instrument]
-fn initialize_logger() {
-    tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
-        .init();
-
-    info!("Initialized logger.");
 }

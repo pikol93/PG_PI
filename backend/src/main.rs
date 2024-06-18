11e6e3c94ea12ca34b3c8022ea3a7f391d40f1a7
pikol93 <piotrdampc93@gmail.com>
2024-06-18 21:06:06 +0200

    PI-5 Moved configuration related code to another file

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,32 +1,19 @@
+mod configuration;
+
+use crate::configuration::Configuration;
 use actix_web::middleware::Logger;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use color_eyre::Result;
 use dotenvy::dotenv;
-use envy::from_env;
-use serde::Deserialize;
-use tracing::log::trace;
-use tracing::{info, instrument};
+use tracing::{debug, info, instrument};
 use tracing_subscriber::filter::EnvFilter;
-
-#[derive(Deserialize, Debug)]
-struct Configuration {
-    host: String,
-    port: u16,
-}
-
-impl Configuration {
-    pub fn new_from_env() -> Result<Configuration> {
-        let result = from_env::<Configuration>()?;
-        trace!("Read environment configuration: {:?}", result);
-        Ok(result)
-    }
-}
 
 #[actix_web::main]
 async fn main() -> Result<()> {
     let _ = dotenv();
     initialize_logger();
     let configuration = Configuration::new_from_env()?;
+    debug!("Read environment configuration: {:?}", configuration);
 
     HttpServer::new(|| App::new().wrap(Logger::default()).service(health_check))
         .bind((configuration.host, configuration.port))?

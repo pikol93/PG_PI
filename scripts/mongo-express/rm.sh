c7c9c53851230222fa3f95c4a8ebce4727bd6e10
pikol93 <piotrdampc93@gmail.com>
2024-07-01 19:04:28 +0200

    PI-19 Add mongo-express scripts

---
+++ scripts/mongo-express/rm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ./var.sh
+
+CONTAINER_ID=$(docker ps -aq --filter name=${CONTAINER_NAME})
+if [[ -z "${CONTAINER_ID}" ]]; then
+  echo "Cannot remove. No container found."
+  exit
+fi
+
+echo "Removing container $CONTAINER_ID"
+docker rm $CONTAINER_ID
---
+++ scripts/mongo-express/var.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+CONTAINER_NAME=pi-mongo-express
+MONGO_SERVER=172.17.0.1:27017

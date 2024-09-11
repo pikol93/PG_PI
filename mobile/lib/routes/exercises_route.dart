1cf346d54b215346f6ac7658de551933759c1303
pikol93 <piotrdampc93@gmail.com>
2024-09-11 16:34:49 +0200

    PI-33 Add draft exercises, home and tracks routes

---
+++ mobile/lib/routes/exercises_route.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ExercisesRoute extends StatelessWidget {
+  const ExercisesRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("exercises route"),
+    );
+  }
+}

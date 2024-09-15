f2a77cb9f1837ec89c945711beab32a55d84bc4f
pikol93 <piotrdampc93@gmail.com>
2024-09-15 15:42:34 +0200

    PI-33 Add SharedPreferencesAsyncExtension

---
+++ mobile/lib/utility/shared_preferences.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+extension SharedPreferencesAsyncExtension on SharedPreferencesAsync {
+  Future<T?> getFromJson<T>(
+    String key,
+    T? Function(Map<String, Object?>) constructor,
+  ) async {
+    final jsonString = await this.getString(key);
+    if (jsonString == null) {
+      print("No stored string value for key $key");
+      return null;
+    }
+
+    final map = jsonDecode(jsonString);
+    final constructed = constructor(map);
+    if (map == null) {
+      print(
+          "Could not construct object of type $T from key $key and string $json");
+    }
+
+    return constructed;
+  }
+}

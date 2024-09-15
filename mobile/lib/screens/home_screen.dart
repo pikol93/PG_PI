0900606bec186fda10879b81952b7ac6b8582140
pikol93 <piotrdampc93@gmail.com>
2024-09-15 15:43:32 +0200

    PI-33 Change home screen to output username

--- mobile/lib/screens/home_screen.dart
+++ mobile/lib/screens/home_screen.dart
@@ -1,20 +1,32 @@
 import 'package:awesome_flutter_extensions/awesome_flutter_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pi_mobile/app_navigation_drawer.dart';
+import 'package:pi_mobile/provider/auth_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       drawer: const AppNavigationDrawer(),
       appBar: AppBar(
         backgroundColor: context.colors.scaffoldBackground,
         title: const Text("Home"),
       ),
-      body: const Center(
-        child: Text("home route"),
+      body: Center(
+        child: switch (ref.watch(authProvider)) {
+          AsyncData(:final value) => () {
+              if (value == null) {
+                return const Text("Invalid state. Could not read auth data.");
+              } else {
+                return Text("Hello ${value.username}");
+              }
+            }(),
+          AsyncError() => const Text("Error while loading state."),
+          _ => const CircularProgressIndicator(),
+        },
       ),
     );
   }

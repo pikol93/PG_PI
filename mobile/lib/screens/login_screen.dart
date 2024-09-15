640ac67776a1e10547ddf00c692569872c067d70
pikol93 <piotrdampc93@gmail.com>
2024-09-15 16:32:15 +0200

    PI-33 Change login screen to allow logging in

--- mobile/lib/screens/login_screen.dart
+++ mobile/lib/screens/login_screen.dart
@@ -1,8 +1,24 @@
 import 'package:awesome_flutter_extensions/awesome_flutter_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pi_mobile/provider/auth_provider.dart';
+import 'package:pi_mobile/routes.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  ConsumerState<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends ConsumerState<LoginScreen> {
+  final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    textController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +27,36 @@ class LoginScreen extends StatelessWidget {
         backgroundColor: context.colors.scaffoldBackground,
         title: const Text("Login"),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           children: [
-            Text("login route"),
-            TextField(),
+            const Text("login route"),
+            TextField(
+              controller: textController,
+            ),
+            ElevatedButton(
+              onPressed: _onLoginButtonPressed,
+              child: const Text("Log in"),
+            )
           ],
         ),
       ),
     );
+  }
+
+  void _onLoginButtonPressed() async {
+    final username = textController.text;
+    if (username.isEmpty) {
+      print("Cannot log in with empty username.");
+      return;
+    }
+
+    print("Login button pressed. Username = $username");
+    await ref.read(authProvider.notifier).logIn(username);
+
+    // TODO: As of now, every attempt to login is successful. Handle cases where logging in fails.
+    if (mounted) {
+      const RootRoute().go(context);
+    }
   }
 }

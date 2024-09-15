6402c62fbfabe66671ada81dfa9d2fd428a40064
pikol93 <piotrdampc93@gmail.com>
2024-09-15 15:55:43 +0200

    PI-33 Add ability to log off via the settings screen

--- mobile/lib/screens/settings_screen.dart
+++ mobile/lib/screens/settings_screen.dart
@@ -1,21 +1,34 @@
 import 'package:awesome_flutter_extensions/awesome_flutter_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pi_mobile/app_navigation_drawer.dart';
+import 'package:pi_mobile/provider/auth_provider.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends ConsumerWidget {
   const SettingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       drawer: const AppNavigationDrawer(),
       appBar: AppBar(
         backgroundColor: context.colors.scaffoldBackground,
         title: const Text("Settings"),
       ),
-      body: const Center(
-        child: Text("settings route"),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => _onLogOffPressed(ref),
+              child: const Text("Log off"),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void _onLogOffPressed(WidgetRef ref) {
+    ref.read(authProvider.notifier).logOff();
   }
 }

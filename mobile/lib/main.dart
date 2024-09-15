fb35072e71178dbe8a6f1916274294f3db236b24
pikol93 <piotrdampc93@gmail.com>
2024-09-15 15:43:14 +0200

    PI-33 Wrap app router with ProviderScope

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pi_mobile/routes.dart';
 
 void main() {
   runApp(
-    MaterialApp.router(
-      title: 'PG PI',
-      routerConfig: GoRouter(routes: $appRoutes),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    ProviderScope(
+      child: MaterialApp.router(
+        title: 'PG PI',
+        routerConfig: GoRouter(routes: $appRoutes),
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
       ),
     ),
   );

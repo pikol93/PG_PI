b118461d4393ad39e0c545760ea775edc0a7bdb4
pikol93 <piotrdampc93@gmail.com>
2024-09-19 20:13:24 +0200

    PI-38 Add setting to change server address

--- mobile/lib/widgets/settings/settings_body.dart
+++ mobile/lib/widgets/settings/settings_body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pi_mobile/provider/auth_provider.dart';
+import 'package:pi_mobile/provider/connection_settings_provider.dart';
 import 'package:pi_mobile/routes.dart';
 import 'package:pi_mobile/widgets/settings/setting_button.dart';
+import 'package:pi_mobile/widgets/settings/setting_text.dart';
 
 class SettingsBody extends StatelessWidget {
   const SettingsBody({super.key});
@@ -12,10 +14,34 @@ class SettingsBody extends StatelessWidget {
     return const SingleChildScrollView(
       child: Column(
         children: [
+          _ChangeServerAddressSetting(),
           _LogOffSetting(),
         ],
       ),
     );
+  }
+}
+
+class _ChangeServerAddressSetting extends ConsumerWidget {
+  const _ChangeServerAddressSetting();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(connectionSettingsProvider).when(
+          data: (data) => SettingText(
+            icon: Icons.computer,
+            title: "Server address",
+            alertTitle: "Enter new server address",
+            currentValue: data.serverAddress,
+            onConfirmed: (value) => _onConfirmed(ref, value),
+          ),
+          error: (obj, stack) => const Text("An unexpected error occurred."),
+          loading: () => const CircularProgressIndicator(),
+        );
+  }
+
+  void _onConfirmed(WidgetRef ref, String newValue) {
+    ref.read(connectionSettingsProvider.notifier).updateServerAddress(newValue);
   }
 }
 

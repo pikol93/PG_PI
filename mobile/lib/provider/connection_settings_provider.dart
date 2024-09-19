8629ba843b9469c0a5499a2bd0f9ee7d130428ed
pikol93 <piotrdampc93@gmail.com>
2024-09-19 17:45:35 +0200

    PI-38 Add ConnectionSettings provider

---
+++ mobile/lib/provider/connection_settings_provider.dart
@@ -0,0 +1,30 @@
+import 'package:pi_mobile/data/connection_settings_state.dart';
+import 'package:pi_mobile/logger.dart';
+import 'package:pi_mobile/utility/shared_preferences.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part "connection_settings_provider.g.dart";
+
+@riverpod
+class ConnectionSettings extends _$ConnectionSettings with Logger {
+  static const keyName = "connection_settings";
+
+  @override
+  Future<ConnectionSettingsState> build() async {
+    final preferences = SharedPreferencesAsync();
+    return await preferences.getFromJson(
+            keyName, ConnectionSettingsState.fromJson) ??
+        const ConnectionSettingsState(serverAddress: "server.xyz");
+  }
+
+  Future<void> updateServerAddress(String serverAddress) async {
+    // TODO: This operation probably does not require us to write and read from shared preferences.
+    logger.debug("Setting server address as $serverAddress...");
+    final preferences = SharedPreferencesAsync();
+    await preferences.setToJson(
+        keyName, ConnectionSettingsState(serverAddress: serverAddress));
+
+    ref.invalidateSelf();
+  }
+}

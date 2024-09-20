dd19ddaee6574e938b9c83f03508602523545a68
pikol93 <piotrdampc93@gmail.com>
2024-09-20 19:52:43 +0200

    PI-41 Add required I18N initialization and wrapped app with a TranslationProvider

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pi_mobile/i18n/strings.g.dart';
 import 'package:pi_mobile/logger.dart';
 import 'package:pi_mobile/routes.dart';
 import 'package:loggy/loggy.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // TODO: Allow changing the locale
+  LocaleSettings.useDeviceLocale();
   Loggy.initLoggy(
     logPrinter: const LoggerPrinter(),
   );
 
   runApp(
     ProviderScope(
-      child: MaterialApp.router(
-        title: 'PG PI',
-        routerConfig: GoRouter(routes: $appRoutes),
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      child: TranslationProvider(
+        child: MaterialApp.router(
+          title: 'PG PI',
+          routerConfig: GoRouter(routes: $appRoutes),
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+          ],
+          supportedLocales: AppLocaleUtils.supportedLocales,
         ),
       ),
     ),

fb6d7cbc62aff52b1b4a34c1bded2638e1946f8a
pikol93 <piotrdampc93@gmail.com>
2024-09-21 12:09:41 +0200

    PI-43 Make StoredLocaleService::keyName public

--- mobile/lib/service/stored_locale_service.dart
+++ mobile/lib/service/stored_locale_service.dart
@@ -5,7 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 // TODO: This class serves as a workaround for riverpod not playing nicely with slang. If possible,
 // rework this class in the future.
 class StoredLocaleService {
-  static const _keyName = "stored_locale";
+  static const keyName = "stored_locale";
 
   static Future<void> initialize() async {
     await _readAndUpdateLocale();
@@ -16,20 +16,20 @@ class StoredLocaleService {
     logDebug("Updating locale to $rawLocale");
 
     final preferences = SharedPreferencesAsync();
-    await preferences.setString(_keyName, locale.name);
+    await preferences.setString(keyName, locale.name);
 
     await _readAndUpdateLocale();
   }
 
-  static Future<void> resetSavedLocale(AppLocale locale) async {
+  static Future<void> resetSavedLocale() async {
     final preferences = SharedPreferencesAsync();
-    await preferences.remove(_keyName);
+    await preferences.remove(keyName);
     await _readAndUpdateLocale();
   }
 
   static Future<void> _readAndUpdateLocale() async {
     final preferences = SharedPreferencesAsync();
-    final rawLocale = await preferences.getString(_keyName);
+    final rawLocale = await preferences.getString(keyName);
     logDebug("Read locale: \"$rawLocale\"");
 
     if (rawLocale != null) {

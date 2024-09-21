af5c9ca0ec37515335a5e20bf248b9e4fb744260
pikol93 <piotrdampc93@gmail.com>
2024-09-21 09:26:30 +0200

    PI-43 Add StoredLocaleService

---
+++ mobile/lib/service/stored_locale_service.dart
@@ -0,0 +1,35 @@
+import 'package:loggy/loggy.dart';
+import 'package:pi_mobile/i18n/strings.g.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// TODO: This class serves as a workaround for riverpod not playing nicely with slang. If possible,
+// rework this class in the future.
+class StoredLocaleService {
+  static const _keyName = "stored_locale";
+
+  static Future<void> initialize() async {
+    _readAndUpdateLocale();
+  }
+
+  static Future<void> saveAndUpdateLocale(AppLocale locale) async {
+    final rawLocale = locale.name;
+    logDebug("Updating locale to $rawLocale");
+
+    final preferences = SharedPreferencesAsync();
+    await preferences.setString(_keyName, locale.name);
+
+    await _readAndUpdateLocale();
+  }
+
+  static Future<void> _readAndUpdateLocale() async {
+    final preferences = SharedPreferencesAsync();
+    final rawLocale = await preferences.getString(_keyName);
+    logDebug("Read locale: \"$rawLocale\"");
+
+    if (rawLocale != null) {
+      LocaleSettings.setLocaleRaw(rawLocale);
+    } else {
+      LocaleSettings.useDeviceLocale();
+    }
+  }
+}

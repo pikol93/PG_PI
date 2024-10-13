5754cfeae45f2dfa64acca02b5977941646f609c
pikol93 <piotrdampc93@gmail.com>
2024-10-13 16:33:10 +0200

    PI-55 Implement TimeSortedTracks provider

--- mobile/lib/provider/time_sorted_tracks_provider.dart
+++ mobile/lib/provider/time_sorted_tracks_provider.dart
@@ -1,5 +1,6 @@
-import "package:pi_mobile/data/location.dart";
+import "package:collection/collection.dart";
 import "package:pi_mobile/data/track.dart";
+import "package:pi_mobile/provider/tracks_provider.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
 part "time_sorted_tracks_provider.g.dart";
@@ -9,58 +10,8 @@ part "time_sorted_tracks_provider.g.dart";
 @Riverpod(keepAlive: true)
 class TimeSortedTracks extends _$TimeSortedTracks {
   @override
-  Future<List<Track>> build() async => [
-        Track(
-          uuid: "uuid1",
-          startTime: DateTime.now().add(const Duration(minutes: 2)),
-          locations: [
-            Location(
-              dateTime: DateTime.now()
-                  .add(const Duration(minutes: 2))
-                  .add(const Duration(seconds: 5)),
-              longitude: 0.1,
-              latitude: 0.2,
-            ),
-          ],
-        ),
-        Track(
-          uuid: "uuid2",
-          startTime: DateTime.now().add(const Duration(hours: 2)),
-          locations: [
-            Location(
-              dateTime: DateTime.now()
-                  .add(const Duration(hours: 2))
-                  .add(const Duration(seconds: 5)),
-              longitude: 0.1,
-              latitude: 0.2,
-            ),
-          ],
-        ),
-        Track(
-          uuid: "uuid3",
-          startTime: DateTime.now().add(const Duration(days: 2)),
-          locations: [
-            Location(
-              dateTime: DateTime.now()
-                  .add(const Duration(days: 2))
-                  .add(const Duration(seconds: 5)),
-              longitude: 0.1,
-              latitude: 0.2,
-            ),
-          ],
-        ),
-        Track(
-          uuid: "uuid4",
-          startTime: DateTime.now().add(const Duration(days: 37)),
-          locations: [
-            Location(
-              dateTime: DateTime.now()
-                  .add(const Duration(days: 37))
-                  .add(const Duration(seconds: 5)),
-              longitude: 0.1,
-              latitude: 0.2,
-            ),
-          ],
-        ),
-      ];
+  Future<List<Track>> build() async {
+    final tracks = await ref.watch(tracksProvider.future);
+    return tracks.sortedBy((track) => track.startTime).reversed.toList();
+  }
 }

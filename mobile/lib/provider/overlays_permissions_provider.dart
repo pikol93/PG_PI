a1060da0f51d540fa08a4716268de1f959a35623
pikol93 <piotrdampc93@gmail.com>
2024-10-13 15:42:19 +0200

    PI-55 Add a check for iOS cases in OverlaysPermission provider

--- mobile/lib/provider/overlays_permissions_provider.dart
+++ mobile/lib/provider/overlays_permissions_provider.dart
@@ -1,3 +1,5 @@
+import "dart:io";
+
 import "package:flutter_foreground_task/flutter_foreground_task.dart";
 import "package:pi_mobile/logger.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
@@ -8,6 +10,12 @@ part "overlays_permissions_provider.g.dart";
 class OverlaysPermissions extends _$OverlaysPermissions with Logger {
   @override
   Future<bool> build() async {
+    if (Platform.isAndroid) {
+      // TODO: iOS does not require overlays permission. This is not an
+      //  equivalent to acquiring such permission. Handle this case.
+      return true;
+    }
+
     final overlaysPermission = await FlutterForegroundTask.canDrawOverlays;
     logger.debug("Overlays permission: $overlaysPermission");
 

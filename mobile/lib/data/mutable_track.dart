c2ce3cb7af80afaaacafbe08bfc49ecba1193fae
pikol93 <piotrdampc93@gmail.com>
2024-10-13 11:38:11 +0200

    PI-55 Add MutableTrack::bakeWithTruncatedLocations

--- mobile/lib/data/mutable_track.dart
+++ mobile/lib/data/mutable_track.dart
@@ -20,4 +20,24 @@ class MutableTrack {
         startTime: startTime,
         locations: locations,
       );
+
+  Track bakeWithTruncatedLocations() {
+    const truncateThreshold = 10.0;
+    final locationsCopy = locations.toList();
+    for (var i = locationsCopy.length - 2; i >= 1; i--) {
+      final locationA = locationsCopy[i - 1];
+      final locationB = locationsCopy[i + 1];
+      final distance = Location.getMetersBetweenLocations(locationA, locationB);
+
+      if (distance < truncateThreshold) {
+        locationsCopy.removeAt(i);
+      }
+    }
+
+    return Track(
+      uuid: uuid,
+      startTime: startTime,
+      locations: locationsCopy,
+    );
+  }
 }

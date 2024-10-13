24903937b261a1aff948db2897b9f49ceb95dc9b
pikol93 <piotrdampc93@gmail.com>
2024-10-13 11:37:58 +0200

    PI-55 Add Utility methods to Location

--- mobile/lib/data/location.dart
+++ mobile/lib/data/location.dart
@@ -1,4 +1,5 @@
 import "package:freezed_annotation/freezed_annotation.dart";
+import "package:latlong2/latlong.dart";
 
 part "location.g.dart";
 
@@ -6,6 +7,8 @@ part "location.freezed.dart";
 
 @freezed
 class Location with _$Location {
+  static const Distance distance = Distance();
+
   const factory Location({
     required DateTime dateTime,
     required double longitude,
@@ -16,4 +19,21 @@ class Location with _$Location {
     Map<String, Object?> json,
   ) =>
       _$LocationFromJson(json);
+
+  static double getVelocityBetweenLocations(Location a, Location b) {
+    final distance = getMetersBetweenLocations(a, b);
+    final duration = getSecondsBetweenLocations(a, b);
+    return distance / duration;
+  }
+
+  static double getMetersBetweenLocations(Location a, Location b) {
+    final positionA = LatLng(a.latitude, a.longitude);
+    final positionB = LatLng(b.latitude, b.longitude);
+
+    return distance.as(LengthUnit.Meter, positionA, positionB);
+  }
+
+  static double getSecondsBetweenLocations(Location a, Location b) =>
+      (b.dateTime.millisecondsSinceEpoch - a.dateTime.millisecondsSinceEpoch) /
+      1000.0;
 }

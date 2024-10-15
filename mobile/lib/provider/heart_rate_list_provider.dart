74fb82f53611e12683d6c2c8b552fa1ecfb36378
pikol93 <piotrdampc93@gmail.com>
2024-10-15 14:17:29 +0200

    PI-66 Add HeartRateList::generateHeartRateData

--- mobile/lib/provider/heart_rate_list_provider.dart
+++ mobile/lib/provider/heart_rate_list_provider.dart
@@ -1,7 +1,9 @@
 import "dart:convert";
+import "dart:math";
 
 import "package:pi_mobile/data/heart_rate_entry.dart";
 import "package:pi_mobile/logger.dart";
+import "package:pi_mobile/utility/random.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 import "package:shared_preferences/shared_preferences.dart";
 
@@ -19,7 +21,7 @@ class HeartRateList extends _$HeartRateList with Logger {
         .map((json) => HeartRateEntry.fromJson(jsonDecode(json)))
         .toList();
 
-    logger.debug("Read ${tracks.length} tracks");
+    logger.debug("Read ${tracks.length} heart rate instances");
     return tracks;
   }
 
@@ -32,5 +34,34 @@ class HeartRateList extends _$HeartRateList with Logger {
     await preferences.setStringList(_keyName, jsonList);
 
     ref.invalidateSelf();
+  }
+
+  Future<void> generateHeartRateData() async {
+    final random = Random();
+    const minHeartRate = 65;
+    const maxHeartRate = 75;
+    const trend = -10;
+    const processLength = Duration(days: 210);
+    final startTime = DateTime.now().subtract(processLength);
+    final endTime = DateTime.now();
+    const durationMin = Duration(days: 2);
+    const durationMax = Duration(days: 5);
+
+    var currentTime = startTime;
+    while (currentTime.isBefore(endTime)) {
+      final nextDuration = random.nextDuration(durationMin, durationMax);
+      currentTime = currentTime.add(nextDuration);
+      final progress =
+          nextDuration.inMilliseconds / processLength.inMilliseconds;
+      final trendIncrease = progress * trend;
+      final baseHeartRate = random.nextRange(minHeartRate, maxHeartRate);
+      final actualHeartRate = baseHeartRate + trendIncrease;
+      final entry = HeartRateEntry(
+        dateTime: currentTime,
+        beatsPerMinute: actualHeartRate,
+      );
+
+      await addEntry(entry);
+    }
   }
 }

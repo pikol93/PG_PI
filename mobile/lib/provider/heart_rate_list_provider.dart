d3b40aeaf3c3e29fe8ea9f9dd169d508a751366b
pikol93 <piotrdampc93@gmail.com>
2024-10-15 13:35:39 +0200

    PI-66 Implement HeartRateList provider

--- mobile/lib/provider/heart_rate_list_provider.dart
+++ mobile/lib/provider/heart_rate_list_provider.dart
@@ -1,10 +1,36 @@
+import "dart:convert";
+
 import "package:pi_mobile/data/heart_rate_entry.dart";
+import "package:pi_mobile/logger.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
+import "package:shared_preferences/shared_preferences.dart";
 
 part "heart_rate_list_provider.g.dart";
 
 @Riverpod(keepAlive: true)
-class HeartRateList extends _$HeartRateList {
+class HeartRateList extends _$HeartRateList with Logger {
+  static const _keyName = "heart_rate";
+
   @override
-  Future<List<HeartRateEntry>> build() async => [];
+  Future<List<HeartRateEntry>> build() async {
+    final preferences = SharedPreferencesAsync();
+    final jsonList = await preferences.getStringList(_keyName) ?? [];
+    final tracks = jsonList
+        .map((json) => HeartRateEntry.fromJson(jsonDecode(json)))
+        .toList();
+
+    logger.debug("Read ${tracks.length} tracks");
+    return tracks;
+  }
+
+  Future<void> addEntry(HeartRateEntry entry) async {
+    final list = await ref.read(heartRateListProvider.future);
+    list.add(entry);
+
+    final preferences = SharedPreferencesAsync();
+    final jsonList = list.map(jsonEncode).toList();
+    await preferences.setStringList(_keyName, jsonList);
+
+    ref.invalidateSelf();
+  }
 }

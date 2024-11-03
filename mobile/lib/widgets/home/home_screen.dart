8296952112544f2c9861f354fc7d97be0028dff7
pikol93 <piotrdampc93@gmail.com>
2024-11-03 16:46:49 +0100

    PI-78 Replace home screen content

--- mobile/lib/widgets/home/home_screen.dart
+++ mobile/lib/widgets/home/home_screen.dart
@@ -2,7 +2,6 @@ import "package:awesome_flutter_extensions/awesome_flutter_extensions.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:pi_mobile/i18n/strings.g.dart";
-import "package:pi_mobile/provider/auth_provider.dart";
 import "package:pi_mobile/widgets/common/app_navigation_drawer.dart";
 
 class HomeScreen extends ConsumerWidget {
@@ -15,18 +14,8 @@ class HomeScreen extends ConsumerWidget {
           backgroundColor: context.colors.scaffoldBackground,
           title: Text(context.t.home.title),
         ),
-        body: Center(
-          child: switch (ref.watch(authProvider)) {
-            AsyncData(:final value) => () {
-                if (value == null) {
-                  return const Text("Invalid state. Could not read auth data.");
-                } else {
-                  return Text("Hello ${value.username}");
-                }
-              }(),
-            AsyncError() => const Text("Error while loading state."),
-            _ => const CircularProgressIndicator(),
-          },
+        body: const Center(
+          child: Text("TODO: Home screen"),
         ),
       );
 }

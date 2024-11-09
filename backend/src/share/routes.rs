77419aefd32e2e13ec1981217d8d110492008fc9
pikol93 <piotrdampc93@gmail.com>
2024-11-09 15:43:27 +0100

    PI-80 Add expiration date to share request

--- backend/src/share/routes.rs
+++ backend/src/share/routes.rs
@@ -3,18 +3,47 @@ use actix_web::{
     web::{Data, Json},
     HttpResponse, Responder,
 };
+use chrono::{Duration, Utc};
 use tracing::warn;
 
-use crate::share::model::ShareResponse;
+use crate::share::model::{ShareRequest, ShareResponse};
 
-use super::{model::DataToShare, service::SharedDataService};
+use super::service::SharedDataService;
+
+const MIN_VALIDITY_DURATION: Duration = Duration::days(1);
+const MAX_VALIDITY_DURATION: Duration = Duration::days(31);
 
 #[post("/share")]
 pub async fn route_share(
     shared_data_service: Data<SharedDataService>,
-    shared_data: Json<DataToShare>,
+    shared_data: Json<ShareRequest>,
 ) -> impl Responder {
-    let save_result = shared_data_service.save(shared_data.into_inner()).await;
+    let ShareRequest {
+        validity_millis,
+        data_to_share,
+    } = shared_data.into_inner();
+
+    let validity_duration = Duration::milliseconds(validity_millis as i64);
+    if validity_duration < MIN_VALIDITY_DURATION {
+        return HttpResponse::BadRequest().body("Validity time is too small.");
+    }
+
+    if validity_duration > MAX_VALIDITY_DURATION {
+        return HttpResponse::BadRequest().body("Validity time is too large.");
+    }
+
+    let time = Utc::now();
+    let Some(expiration_date) = time.checked_add_signed(validity_duration) else {
+        warn!(
+            "Invalid expiration date. {:?} {:?}",
+            time, validity_duration
+        );
+        return HttpResponse::InternalServerError().body("Invalid date.");
+    };
+
+    let save_result = shared_data_service
+        .save(expiration_date, data_to_share)
+        .await;
     let object_id = match save_result {
         Ok(object_id) => object_id,
         Err(error) => {
--- backend/src/share/service.rs
+++ backend/src/share/service.rs
@@ -1,5 +1,10 @@
+use chrono::{DateTime, Utc};
 use eyre::{OptionExt, Result};
-use mongodb::{bson::oid::ObjectId, Client, Collection};
+use mongodb::{
+    bson::{doc, oid::ObjectId},
+    Client, Collection,
+};
+use serde::{Deserialize, Serialize};
 
 use super::model::DataToShare;
 
@@ -8,15 +13,30 @@ pub struct SharedDataService {
     client: Client,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct ShareDocument {
+    expiration_date: DateTime<Utc>,
+    data: DataToShare,
+}
+
 impl SharedDataService {
     pub fn new_with_client(client: Client) -> Self {
         Self { client }
     }
 
-    pub async fn save(&self, data: DataToShare) -> Result<ObjectId> {
+    pub async fn save(
+        &self,
+        expiration_date: DateTime<Utc>,
+        data: DataToShare,
+    ) -> Result<ObjectId> {
+        let document = ShareDocument {
+            expiration_date,
+            data,
+        };
+
         let result = self
             .get_shared_data_collection()
-            .insert_one(data)
+            .insert_one(document)
             .await?
             .inserted_id
             .as_object_id()
@@ -25,7 +45,7 @@ impl SharedDataService {
         Ok(result)
     }
 
-    fn get_shared_data_collection(&self) -> Collection<DataToShare> {
+    fn get_shared_data_collection(&self) -> Collection<ShareDocument> {
         self.client.database("pg_pi").collection("shared_data")
     }
 }

967b4c6570d7e161b875d06c5eca66056de435c4
pikol93 <piotrdampc93@gmail.com>
2024-11-11 22:23:44 +0100

    PI-29 Implement draft HeartRateCalendarEventDetails

--- mobile/lib/widgets/calendar/calendar_event_details.dart
+++ mobile/lib/widgets/calendar/calendar_event_details.dart
@@ -5,7 +5,7 @@ import "package:pi_mobile/data/collections/track.dart";
 import "package:pi_mobile/provider/date_formatter_provider.dart";
 import "package:pi_mobile/utility/duration.dart";
 
-class HeartRateCalendarEventDetails extends StatelessWidget {
+class HeartRateCalendarEventDetails extends ConsumerWidget {
   final HeartRate heartRate;
 
   const HeartRateCalendarEventDetails({
@@ -14,11 +14,38 @@ class HeartRateCalendarEventDetails extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => Text(
-        "heart rate calendar day details:"
-        " ${heartRate.time}"
-        " ${heartRate.beatsPerMinute}",
-      );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final dateFormatter = ref.read(dateFormatterProvider);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 3.0),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(5.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(dateFormatter.hourMinute(heartRate.time)),
+                  const SizedBox(width: 8.0),
+                  const Icon(Icons.favorite),
+                ],
+              ),
+              Text("${heartRate.beatsPerMinute.toInt()} BPM"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class TrackCalendarEventDetails extends ConsumerWidget {

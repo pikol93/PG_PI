fae8fc5e8e3ffde1f679f21a6881eb5fb82c598f
pikol93 <piotrdampc93@gmail.com>
2024-11-17 10:05:13 +0100

    PI-99 Fix routes not being initialized correctly

--- mobile/lib/routing/router.dart
+++ mobile/lib/routing/router.dart
@@ -1,7 +1,10 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:go_router/go_router.dart";
-import "package:pi_mobile/routing/routes.dart";
+import "package:pi_mobile/routing/routes.dart" as base;
+import "package:pi_mobile/routing/routes_exercises.dart" as exercises;
+import "package:pi_mobile/routing/routes_heart_rate.dart" as heart_rate;
+import "package:pi_mobile/routing/routes_tracks.dart" as tracks;
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
 part "router.g.dart";
@@ -13,8 +16,13 @@ GoRouter router(Ref ref) {
   final router = GoRouter(
     debugLogDiagnostics: true,
     navigatorKey: routerKey,
-    initialLocation: const SplashRoute().location,
-    routes: $appRoutes,
+    initialLocation: const base.SplashRoute().location,
+    routes: [
+      ...base.$appRoutes,
+      ...exercises.$appRoutes,
+      ...heart_rate.$appRoutes,
+      ...tracks.$appRoutes,
+    ],
   );
 
   ref.onDispose(router.dispose);

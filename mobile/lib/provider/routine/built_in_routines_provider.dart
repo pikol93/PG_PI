67c80c9200987b2f107ec2152734425766092e76
pikol93 <piotrdampc93@gmail.com>
2024-11-22 18:53:30 +0100

    PI-101 Add translations to builtInRoutines provider

--- mobile/lib/provider/routine/built_in_routines_provider.dart
+++ mobile/lib/provider/routine/built_in_routines_provider.dart
@@ -1,25 +1,39 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:fpdart/fpdart.dart";
 import "package:pi_mobile/data/routine/built_in_routines.dart" as temp;
 import "package:pi_mobile/data/routine/routine.dart";
+import "package:pi_mobile/provider/routine/routine_i18n_provider.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
 part "built_in_routines_provider.g.dart";
 
 @riverpod
-List<Routine> builtInRoutines(Ref ref) => temp.builtInRoutines
-    .map(
-      (routine) => Routine(
-        id: routine.id,
-        name: routine.identifier, // TODO: I18N
-        workouts: routine.workouts
-            .map(
-              (workout) => Workout(
-                id: workout.id,
-                name: workout.identifier, // TODO: I18N
-                exercises: workout.exercises,
-              ),
-            )
-            .toList(),
-      ),
-    )
-    .toList();
+List<Routine> builtInRoutines(Ref ref) {
+  final routineI18N = ref.watch(routineI18NProvider);
+
+  return temp.builtInRoutines
+      .map(
+        (routine) => Routine(
+          id: routine.id,
+          name: routineI18N
+              .name(routine.identifier)
+              .getOrElse(() => "MISSING TRANSLATION: ${routine.identifier}"),
+          workouts: routine.workouts
+              .map(
+                (workout) => Workout(
+                  id: workout.id,
+                  name: routineI18N
+                      .workoutName(routine.identifier, workout.identifier)
+                      .getOrElse(
+                        () => "MISSING TRANSLATION: "
+                            "${routine.identifier} "
+                            "${workout.identifier}",
+                      ),
+                  exercises: workout.exercises,
+                ),
+              )
+              .toList(),
+        ),
+      )
+      .toList();
+}

a06ea8b35c9b3aa1e8c528362a4f2be48a73bf90
pikol93 <piotrdampc93@gmail.com>
2024-11-22 16:46:44 +0100

    PI-101 Add Routine, Workout, WorkoutExercise and ExerciseSet

---
+++ mobile/lib/data/routine/routine.dart
@@ -0,0 +1,38 @@
+import "package:freezed_annotation/freezed_annotation.dart";
+
+part "routine.freezed.dart";
+
+@freezed
+class Routine with _$Routine {
+  const factory Routine({
+    required int id,
+    required String name,
+    required List<Workout> workouts,
+  }) = _Routine;
+}
+
+@freezed
+class Workout with _$Workout {
+  const factory Workout({
+    required int id,
+    required String name,
+    required List<WorkoutExercise> exercises,
+  }) = _Workout;
+}
+
+@freezed
+class WorkoutExercise with _$WorkoutExercise {
+  const factory WorkoutExercise({
+    required int exerciseId,
+    required List<ExerciseSet> sets,
+  }) = _WorkoutExercise;
+}
+
+@freezed
+class ExerciseSet with _$ExerciseSet {
+  const factory ExerciseSet({
+    required double intensity,
+    required int reps,
+    @Default(false) bool isAmrap,
+  }) = _ExerciseSet;
+}

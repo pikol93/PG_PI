004f3550900d97668ba95739e931e25cb10736fd
pikol93 <piotrdampc93@gmail.com>
2024-11-24 13:02:58 +0100

    PI-101 Add AppScaffold

---
+++ mobile/lib/widgets/common/scaffold/app_scaffold.dart
@@ -0,0 +1,32 @@
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:pi_mobile/provider/routine/active_session_service_provider.dart";
+import "package:pi_mobile/widgets/routines/common/active_session_bottom_sheet.dart";
+
+class AppScaffold extends ConsumerWidget {
+  final AppBar appBar;
+  final Widget body;
+  final Widget? drawer;
+
+  const AppScaffold({
+    super.key,
+    required this.appBar,
+    required this.body,
+    this.drawer,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) => Scaffold(
+        appBar: appBar,
+        body: body,
+        bottomSheet: ref
+            .watch(activeSessionOrNoneProvider)
+            .map(
+              (activeSession) => ActiveSessionBottomSheet(
+                activeSession: activeSession,
+              ),
+            )
+            .toNullable(),
+        drawer: drawer,
+      );
+}

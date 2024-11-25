19d03ca624f7394fdc44b8abb54b1d300c3cf440
pikol93 <piotrdampc93@gmail.com>
2024-11-25 14:59:44 +0100

    PI-101 Rework ActiveSession

--- mobile/lib/data/routine/active_session.dart
+++ mobile/lib/data/routine/active_session.dart
@@ -1,13 +1,18 @@
+import "package:fpdart/fpdart.dart";
 import "package:freezed_annotation/freezed_annotation.dart";
 
 part "active_session.g.dart";
+
 part "active_session.freezed.dart";
 
 @freezed
 class ActiveSession with _$ActiveSession {
   const factory ActiveSession({
+    required DateTime startingDate,
+    required int routineId,
     required int workoutId,
-    required List<SessionExercise> exercises,
+    required List<ActiveSessionExercise> exercises,
+    @Default(Option.none()) Option<int> currentExerciseIndex,
   }) = _ActiveSession;
 
   factory ActiveSession.fromJson(Map<String, Object?> json) =>
@@ -15,21 +20,53 @@ class ActiveSession with _$ActiveSession {
 }
 
 @freezed
-class SessionExercise with _$SessionExercise {
-  const factory SessionExercise({
-    required int sessionId,
-  }) = _SessionExercise;
+class ActiveSessionExercise with _$ActiveSessionExercise {
+  const factory ActiveSessionExercise({
+    required int exerciseId,
+    required List<ActiveSessionSet> sets,
+  }) = _ActiveSessionExercise;
 
-  factory SessionExercise.fromJson(Map<String, Object?> json) =>
-      _$SessionExerciseFromJson(json);
+  factory ActiveSessionExercise.fromJson(Map<String, Object?> json) =>
+      _$ActiveSessionExerciseFromJson(json);
 }
 
 @freezed
-class SessionSet with _$SessionSet {
-  const factory SessionSet({
-    required int sessionId,
-  }) = _SessionSet;
+class ActiveSessionSet with _$ActiveSessionSet {
+  const factory ActiveSessionSet({
+    required double expectedIntensity,
+    required int expectedReps,
+    required int expectedRestTimeSeconds,
+    @Default(ActiveSessionSetResultUnion.toBeDone())
+    ActiveSessionSetResultUnion result,
+  }) = _ActiveSessionSet;
 
-  factory SessionSet.fromJson(Map<String, Object?> json) =>
-      _$SessionSetFromJson(json);
+  factory ActiveSessionSet.fromJson(Map<String, Object?> json) =>
+      _$ActiveSessionSetFromJson(json);
+}
+
+@freezed
+sealed class ActiveSessionSetResultUnion with _$ActiveSessionSetResultUnion {
+  const factory ActiveSessionSetResultUnion.completed(
+    ActiveSessionSetResult value,
+  ) = Completed;
+
+  const factory ActiveSessionSetResultUnion.skipped() = Skipped;
+
+  const factory ActiveSessionSetResultUnion.toBeDone() = ToBeDone;
+
+  factory ActiveSessionSetResultUnion.fromJson(Map<String, dynamic> json) =>
+      _$ActiveSessionSetResultUnionFromJson(json);
+}
+
+@freezed
+class ActiveSessionSetResult with _$ActiveSessionSetResult {
+  const factory ActiveSessionSetResult({
+    required double weight,
+    required int reps,
+    required double rpe,
+    required int restTimeSeconds,
+  }) = _ActiveSessionSetResult;
+
+  factory ActiveSessionSetResult.fromJson(Map<String, Object?> json) =>
+      _$ActiveSessionSetResultFromJson(json);
 }

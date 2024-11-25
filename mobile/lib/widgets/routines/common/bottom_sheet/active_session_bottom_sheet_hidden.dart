1383edbb9115996f9eb731013e234224fb18bf32
pikol93 <piotrdampc93@gmail.com>
2024-11-25 15:00:55 +0100

    PI-101 Update active session bottom sheets

--- mobile/lib/widgets/routines/common/bottom_sheet/active_session_bottom_sheet_hidden.dart
+++ mobile/lib/widgets/routines/common/bottom_sheet/active_session_bottom_sheet_hidden.dart
@@ -1,7 +1,14 @@
 import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:fpdart/fpdart.dart";
 import "package:pi_mobile/data/routine/active_session.dart";
+import "package:pi_mobile/provider/exercise_models_provider.dart";
+import "package:pi_mobile/provider/routine/routines_provider.dart";
+import "package:pi_mobile/utility/async_value.dart";
+import "package:pi_mobile/utility/map.dart";
+import "package:pi_mobile/utility/option.dart";
 
-class ActiveSessionBottomSheetHidden extends StatelessWidget {
+class ActiveSessionBottomSheetHidden extends ConsumerWidget {
   final ActiveSession activeSession;
 
   const ActiveSessionBottomSheetHidden({
@@ -10,10 +17,35 @@ class ActiveSessionBottomSheetHidden extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => const Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text("Active session hidden."),
-        ],
-      );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final workoutName = ref
+        .watch(routinesMapProvider)
+        .toOption()
+        .map((map) => map.get(activeSession.routineId))
+        .flatten()
+        .map(
+          (routine) => routine.workouts
+              .where((workout) => workout.id == activeSession.workoutId)
+              .firstOption,
+        )
+        .flatten()
+        .map((workout) => workout.name)
+        .orElse("UNKNOWN");
+
+    final exerciseName = ref
+        .watch(exerciseModelsMapProvider)
+        .toOption()
+        .map((map) => map.get(1000000000))
+        .flatten()
+        .map((model) => model.name)
+        .orElse("");
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(workoutName),
+        Text(exerciseName),
+      ],
+    );
+  }
 }
--- mobile/lib/widgets/routines/common/bottom_sheet/active_session_bottom_sheet_shown.dart
+++ mobile/lib/widgets/routines/common/bottom_sheet/active_session_bottom_sheet_shown.dart
@@ -1,7 +1,16 @@
+import "package:awesome_flutter_extensions/awesome_flutter_extensions.dart";
 import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:fpdart/fpdart.dart";
 import "package:pi_mobile/data/routine/active_session.dart";
+import "package:pi_mobile/logger.dart";
+import "package:pi_mobile/provider/routine/active_session_service_provider.dart";
+import "package:pi_mobile/provider/routine/routines_provider.dart";
+import "package:pi_mobile/utility/async_value.dart";
+import "package:pi_mobile/utility/map.dart";
+import "package:pi_mobile/utility/option.dart";
 
-class ActiveSessionBottomSheetShown extends StatelessWidget {
+class ActiveSessionBottomSheetShown extends ConsumerWidget {
   final ActiveSession activeSession;
 
   const ActiveSessionBottomSheetShown({
@@ -10,10 +19,92 @@ class ActiveSessionBottomSheetShown extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => const Column(
+  Widget build(BuildContext context, WidgetRef ref) => Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text("Active session shown."),
+          const SizedBox(height: 32.0),
+          _Header(activeSession: activeSession),
+          ElevatedButton(
+            onPressed: () => ref
+                .read(activeSessionServiceProvider)
+                .removeMeRemoveMeRemoveMe()
+                .run(),
+            child: const Text("Clear"),
+          ),
         ],
       );
+}
+
+class _Header extends ConsumerWidget with Logger {
+  final ActiveSession activeSession;
+
+  _Header({required this.activeSession});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final workoutName = ref
+        .watch(routinesMapProvider)
+        .toOption()
+        .map((map) => map.get(activeSession.routineId))
+        .flatten()
+        .map(
+          (routine) => routine.workouts
+              .where((workout) => workout.id == activeSession.workoutId)
+              .firstOption,
+        )
+        .flatten()
+        .map((workout) => workout.name)
+        .orElse("Workout");
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: _onHidePressed,
+          icon: const Icon(Icons.expand_more),
+        ),
+        Expanded(
+          child: Text(
+            workoutName,
+            textAlign: TextAlign.center,
+            style: context.textStyles.titleLarge,
+          ),
+        ),
+        PopupMenuButton(
+          itemBuilder: (context) => [
+            PopupMenuItem(
+              onTap: () => _onEndExercisePressed(ref),
+              child: const Text("End exercise"), // TODO: I18N
+            ),
+            PopupMenuItem(
+              onTap: () => _onEndWorkoutPressed(ref),
+              child: const Text("End workout"), // TODO: I18N
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  void _onHidePressed() {
+    logger.debug("Hide pressed.");
+  }
+
+  Future<void> _onEndExercisePressed(WidgetRef ref) async {
+    logger.debug("Skip exercise tapped");
+    await ref
+        .read(activeSessionServiceProvider)
+        .update(
+          activeSession.copyWith(currentExerciseIndex: const Option.none()),
+        )
+        .run();
+  }
+
+  Future<void> _onEndWorkoutPressed(WidgetRef ref) async {
+    logger.debug("End workout pressed");
+    final finishResult =
+        await ref.read(activeSessionServiceProvider).finish().run();
+
+    logger.debug("Active sesion finish result: $finishResult");
+  }
 }

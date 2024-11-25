88fd621e39c43238d5a7f23f6712ef3760638472
pikol93 <piotrdampc93@gmail.com>
2024-11-25 14:59:10 +0100

    PI-101 Add Session collection

---
+++ mobile/lib/data/routine/session.dart
@@ -0,0 +1,28 @@
+import "package:isar/isar.dart";
+
+part "session.g.dart";
+
+@collection
+class Session {
+  Id id = Isar.autoIncrement;
+  @Index(type: IndexType.value)
+  @Index(composite: [CompositeIndex("workoutId")])
+  late int routineId;
+  late int workoutId;
+  @Index(type: IndexType.value)
+  late DateTime startDate;
+  late List<SessionExercise> exercises;
+}
+
+@embedded
+class SessionExercise {
+  late int exerciseId;
+  late List<SessionSet> sets;
+}
+
+@embedded
+class SessionSet {
+  late double weight;
+  late int reps;
+  late double rpe;
+}

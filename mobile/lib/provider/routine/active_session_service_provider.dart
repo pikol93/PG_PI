fb1773e39d0ddf3b9c774e613693bf8a62a55370
pikol93 <piotrdampc93@gmail.com>
2024-11-25 15:00:18 +0100

    PI-101 Rework ActiveSessionService

--- mobile/lib/provider/routine/active_session_service_provider.dart
+++ mobile/lib/provider/routine/active_session_service_provider.dart
@@ -2,12 +2,18 @@ import "dart:convert";
 
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:fpdart/fpdart.dart";
+import "package:loggy/loggy.dart";
 import "package:pi_mobile/data/routine/active_session.dart";
+import "package:pi_mobile/data/routine/session.dart";
 import "package:pi_mobile/logger.dart";
+import "package:pi_mobile/provider/routine/routines_provider.dart";
+import "package:pi_mobile/provider/routine/session_service_provider.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 import "package:shared_preferences/shared_preferences.dart";
 
 part "active_session_service_provider.g.dart";
+
+const _keyName = "active_session";
 
 @riverpod
 Option<ActiveSession> activeSessionOrNone(Ref ref) =>
@@ -18,44 +24,152 @@ Option<ActiveSession> activeSessionOrNone(Ref ref) =>
         );
 
 @riverpod
-Future<Option<ActiveSession>> activeSession(Ref ref) =>
-    ref.watch(activeSessionServiceProvider).read().run();
+Future<Option<ActiveSession>> activeSession(Ref ref) async {
+  final storedJson = await SharedPreferencesAsync().getString(_keyName);
+  if (storedJson == null) {
+    logDebug("No stored active session found.");
+    return const Option.none();
+  }
+
+  try {
+    final activeSession = ActiveSession.fromJson(jsonDecode(storedJson));
+    return Option.of(activeSession);
+  } catch (ex) {
+    logWarning("Could not decode active session. Data: $storedJson");
+    return const Option.none();
+  }
+}
 
 @riverpod
-ActiveSessionService activeSessionService(Ref ref) =>
-    ActiveSessionService(ref: ref);
+ActiveSessionService activeSessionService(Ref ref) => ActiveSessionService(
+      ref: ref,
+    );
 
 class ActiveSessionService with Logger {
-  static const _keyName = "active_session";
-
   final Ref ref;
 
   ActiveSessionService({required this.ref});
 
-  Task<Option<ActiveSession>> read() => Task(() async {
-        final storedJson = await SharedPreferencesAsync().getString(_keyName);
-        if (storedJson == null) {
-          logger.debug("No stored active session found.");
-          return const Option.none();
-        }
+  TaskEither<void, void> initiate(
+    int routineId,
+    int workoutId,
+    Option<int> currentExerciseIndex,
+  ) =>
+      TaskEither.tryCatch(
+        () async {
+          final routinesMap = await ref.read(routinesMapProvider.future);
 
-        try {
-          final activeSession = ActiveSession.fromJson(jsonDecode(storedJson));
-          return Option.of(activeSession);
-        } catch (ex) {
-          logger.warning("Could not decode active session. Data: $storedJson");
-          return const Option.none();
-        }
+          final routine = routinesMap[routineId]!;
+          final workout =
+              routine.workouts.firstWhere((workout) => workout.id == workoutId);
+
+          await finish().run();
+
+          final session = ActiveSession(
+            startingDate: DateTime.now(),
+            routineId: routineId,
+            workoutId: workoutId,
+            currentExerciseIndex: currentExerciseIndex,
+            exercises: workout.exercises
+                .map(
+                  (exercise) => ActiveSessionExercise(
+                    exerciseId: exercise.exerciseId,
+                    sets: exercise.sets
+                        .map(
+                          (set) => ActiveSessionSet(
+                            expectedIntensity: set.intensity,
+                            expectedReps: set.reps,
+                            expectedRestTimeSeconds: 100,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                )
+                .toList(),
+          );
+
+          await _overwrite(session).run();
+        },
+        (error, stackTrace) {
+          logger.warning(
+            "Failed initiating active session. $error, $stackTrace",
+          );
+        },
+      );
+
+  Task<void> update(ActiveSession session) => Task(() async {
+        _overwrite(session);
+        logger.debug("Updated active session");
       });
 
-  Task<void> save(ActiveSession activeSession) => Task(() async {
+  TaskEither<void, void> finish() => TaskEither.tryCatch(
+        () async {
+          final activeSession = await _readState()
+              .map((activeSessionOption) => activeSessionOption.toNullable()!)
+              .run();
+
+          final session = Session()
+            ..startDate = activeSession.startingDate
+            ..routineId = activeSession.routineId
+            ..workoutId = activeSession.workoutId
+            ..exercises = activeSession.exercises
+                .map(
+                  (exercise) => SessionExercise()
+                    ..exerciseId = exercise.exerciseId
+                    ..sets = exercise.sets
+                        .map((set) => set.result)
+                        .map(
+                          (result) =>
+                              result.maybeMap<Option<ActiveSessionSetResult>>(
+                            completed: (item) => Option.of(item.value),
+                            orElse: Option.none,
+                          ),
+                        )
+                        .where((option) => option.isSome())
+                        .map((option) => option.toNullable()!)
+                        .map(
+                          (result) => SessionSet()
+                            ..weight = result.weight
+                            ..reps = result.reps
+                            ..rpe = result.rpe,
+                        )
+                        .toList(),
+                )
+                .where((exercise) => exercise.sets.isNotEmpty)
+                .toList();
+
+          if (session.exercises.isEmpty) {
+            throw StateError("Cannot add session with no exercises.");
+          }
+
+          final sessionService = await ref.read(sessionServiceProvider.future);
+          await sessionService.save(session).run();
+
+          return _clear().run();
+        },
+        (error, stackTrace) {
+          logger.debug("Could not finish active session. $error");
+        },
+      );
+
+  Task<void> removeMeRemoveMeRemoveMe() => _clear();
+
+  Task<void> _overwrite(ActiveSession activeSession) => Task(() async {
+        logger.debug("Saving active session: $activeSession");
         final json = jsonEncode(activeSession);
         await SharedPreferencesAsync().setString(_keyName, json);
-        ref.invalidateSelf();
+        return _invalidateStoredSession().run();
       });
 
-  Task<void> clear() => Task(() async {
+  Task<void> _clear() => Task(() async {
         await SharedPreferencesAsync().remove(_keyName);
-        ref.invalidateSelf();
+        return _invalidateStoredSession().run();
+      });
+
+  Task<Option<ActiveSession>> _readState() =>
+      Task(() => ref.read(activeSessionProvider.future));
+
+  Task<void> _invalidateStoredSession() => Task(() async {
+        ref.invalidate(activeSessionProvider);
       });
 }

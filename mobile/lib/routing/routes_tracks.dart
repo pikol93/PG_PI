b8d59db9fbad843c28144aed1e8bc8c463fa3e32
pikol93 <piotrdampc93@gmail.com>
2024-11-29 16:30:18 +0100

    PI-64 Move permissions screens

--- mobile/lib/routing/routes_tracks.dart
+++ mobile/lib/routing/routes_tracks.dart
@@ -9,11 +9,11 @@ import "package:pi_mobile/data/permissions/notification_permissions_provider.dar
 import "package:pi_mobile/data/permissions/overlays_permissions_provider.dart";
 import "package:pi_mobile/utility/location_permission.dart";
 import "package:pi_mobile/utility/notification_permission.dart";
+import "package:pi_mobile/widgets/tracks/permissions/request_battery_permission_screen.dart";
+import "package:pi_mobile/widgets/tracks/permissions/request_location_permission_screen.dart";
+import "package:pi_mobile/widgets/tracks/permissions/request_notification_permission_screen.dart";
+import "package:pi_mobile/widgets/tracks/permissions/request_overlays_permission_screen.dart";
 import "package:pi_mobile/widgets/tracks/record_track_screen.dart";
-import "package:pi_mobile/widgets/tracks/request_battery_permission_screen.dart";
-import "package:pi_mobile/widgets/tracks/request_location_permission_screen.dart";
-import "package:pi_mobile/widgets/tracks/request_notification_permission_screen.dart";
-import "package:pi_mobile/widgets/tracks/request_overlays_permission_screen.dart";
 import "package:pi_mobile/widgets/tracks/track_details_screen.dart";
 import "package:pi_mobile/widgets/tracks/tracks_screen.dart";
 
